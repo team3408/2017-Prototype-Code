c61ebacb60a5fb960cf2a9ff4ee745fe936f15d8
David Arico <davidarico@me.com>
2017-01-10 19:19:43 -0800

    Added and wrote shooter code

---
+++ Shooter.cpp
@@ -0,0 +1,35 @@
+#include "WPILib.h"
+#include <math.h>
+
+class Robot: public IterativeRobot{
+
+private:
+
+  void RobotInit(){
+    JoystickButton * spinWheel
+    CanTalonSRX * Talon5
+    CanTalonSRX * Talon6
+    int spinWheel = JoystickButton(4);
+  }
+
+  void AutonomousInit(){
+
+  }
+
+  void TeleopInit(){
+    void Shoot(){
+      Talon5 = new CanTalonSRX(5);
+      Talon6 = new CanTalonSRX(6);
+      float talonPower = 0.5;
+      Talon5->Set(talonPower);
+      Talon6->Set(talonPower);
+    }
+
+  }
+
+  void TestPeriodic(){
+    spinWheel->WhileHeld(Shoot());
+  }
+};
+
+START_ROBOT_CLASS(Robot)
