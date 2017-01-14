9159df677d5ac2ba191fa521fa6f78f31d42168e
David Arico <davidarico@me.com>
2017-01-14 14:10:21 -0800

    Update Robot.cpp

--- Robot.cpp
+++ Robot.cpp
@@ -1,47 +1,79 @@
 #include "WPILib.h"
-#include <"CANTalon.h">
+#include <"CanTalon.h">
 
 class Robot: public IterativeRobot
 {
-public:
-	RobotDrive *theDrive;
-	Joystick *leftDriverStick, *rightDriverStick;
-	CANTalon *frontLeft,*frontRight, *backRight ,*backLeft ;
-	frontLeft = new CanTalonSRX(2);//We forgot new
-	frontRight = new CanTalonSRX(1);
-	backLeft = new CanTalonSRX(3);
-	backRight = new CanTalonSRX(4);
-	LiveWindow *lw;
-	
+
+
+private:
+	LiveWindow *lw = LiveWindow::GetInstance();
+	SendableChooser *chooser;
+	const std::string autoNameDefault = "Default";
+	const std::string autoNameCustom = "My Auto";
+	std::string autoSelected;
+	RobotDrive *RoboDrive;
+	Joystick *left, *right;
+	CanTalonSRX *rearLeft, *frontLeft, *rearRight, *frontRight; 
+
 	void RobotInit()
 	{
-		lw = LiveWindow::GetInstance();
+		chooser = new SendableChooser();
+		chooser->AddDefault(autoNameDefault, (void*)&autoNameDefault);
+		chooser->AddObject(autoNameCustom, (void*)&autoNameCustom);
+		SmartDashboard::PutData("Auto Modes", chooser);
+		frontLeft = new CanTalonSRX(0);
+		frontRight = new CanTalonSRX(1);
+		rearLeft = new CanTalonSRX(2);
+		rearRight = new CanTalonSRX(3);
+
+
 	}
 
 
-
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
+	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
+	 * Dashboard, remove all of the chooser code and uncomment the GetString line to get the auto name from the text box
+	 * below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional comparisons to the if-else structure below with additional strings.
+	 * If using the SendableChooser make sure to add them to the chooser code above as well.
+	 */
 	void AutonomousInit()
 	{
+		autoSelected = *((std::string*)chooser->GetSelected());
+		//std::string autoSelected = SmartDashboard::GetString("Auto Selector", autoNameDefault);
+		std::cout << "Auto selected: " << autoSelected << std::endl;
 
+		if(autoSelected == autoNameCustom){
+			//Custom Auto goes here
+		} else {
+			//Default Auto goes here
+		}
 	}
 
 	void AutonomousPeriodic()
 	{
-
+		if(autoSelected == autoNameCustom){
+			//Custom Auto goes here
+		} else {
+			//Default Auto goes here
+		}
 	}
 
 	void TeleopInit()
 	{
-		theDrive = new RobotDrive(frontRight, frontLeft, backLeft, backRight);
-		leftDriverStick = new Joystick(1);
-		rightDriverStick = new Joystick(2);
+		left = new Joystick(1);
+		right = new Joystick(2);
+		RoboDrive = new RobotDrive(frontLeft, rearLeft, frontRight, rearRight);
 	}
+	void TeleopPeriodic(){
 
-	void TeleopPeriodic()
-	{
-
-		theDrive->TankDrive(leftDriverStick,rightDriverStick);
-		
+		frontLeft -> Set(0.2);
+		frontRight -> Set(0.2);
+		rearLeft -> Set(0.2);
+		rearRight ->Set(.2);
+		RoboDrive->TankDrive(left,right);
 	}
 
 	void TestPeriodic()
@@ -51,3 +83,4 @@ public:
 };
 
 START_ROBOT_CLASS(Robot)
+
