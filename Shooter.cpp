5656e75c8e02ca47e7c07f1f22f7c746e2ebacbf
Carter Bowdoin <carterbowdoin@lhsla.org>
2017-01-20 22:04:43 -0800

    Update Shooter.cpp

--- Shooter.cpp
+++ Shooter.cpp
@@ -1,5 +1,20 @@
 #include "WPILib.h"
 #include <math.h>
+//things to do
+//Make shooter move
+//have shooter have multiple power values ie. use 10 buttons
+//Output the motor value
+//Encoder Speed output
+//Test with talon after sparks move
+public bool shoot10;
+public bool shoot20;
+public bool shoot30;
+public bool shoot40;
+public bool shoot50;
+public bool shoot60;
+public bool shoot70;
+public bool shoot80;
+public bool shoot90;
 
 class Robot: public IterativeRobot{
 
